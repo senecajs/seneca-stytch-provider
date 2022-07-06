eecb5d4900f6efbaff3d36db0f2289564c01e5c8
Richard Rodger <richard@ricebridge.com>
2022-07-06 20:35:15 +0100

    v0.1.2

--- dist/trello-provider.d.ts
+++
@@ -1,7 +0,0 @@
-declare type TrelloProviderOptions = {};
-declare function TrelloProvider(this: any, _options: TrelloProviderOptions): {
-    exports: {
-        sdk: () => any;
-    };
-};
-export default TrelloProvider;
