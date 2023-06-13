5b5dfdc78c9cb29629440d646fdc632bcb80b6ea
Alex Milenkovic <jk1143298@gmail.com>
2023-06-13 17:34:40 +0200

    update

--- dist/StytchProvider-doc.js
+++ dist/StytchProvider-doc.js
@@ -1,13 +1,32 @@
 "use strict";
 /* Copyright © 2022 Seneca Project Contributors, MIT License. */
 Object.defineProperty(exports, "__esModule", { value: true });
-const docs = {
+const messages = {
     get_info: {
         desc: 'Get information about the Stytch SDK.',
     },
+    load_user: {
+        desc: 'Load Stytch data into an entity',
+    },
+    save_user: {
+        desc: 'Update/Save Stytch data into an entity',
+    },
+    list_user: {
+        desc: 'List Stytch data into an entity',
+    },
+    remove_user: {
+        desc: 'Remove Stytch data from an entity',
+    }
 };
-exports.default = docs;
+const sections = {};
+exports.default = {
+    messages,
+    sections
+};
 if ('undefined' !== typeof (module)) {
-    module.exports = docs;
+    module.exports = {
+        messages,
+        sections
+    };
 }
 //# sourceMappingURL=StytchProvider-doc.js.map
--- src/StytchProvider-doc.ts
+++ src/StytchProvider-doc.ts
@@ -1,16 +1,37 @@
 /* Copyright © 2022 Seneca Project Contributors, MIT License. */
 
 
-const docs = {
+const messages = {
 
   get_info: {
     desc: 'Get information about the Stytch SDK.',
   },
+  load_user: {
+    desc: 'Load Stytch data into an entity',
+  },
+  save_user: {
+    desc: 'Update/Save Stytch data into an entity',
+  },
+  list_user: {
+    desc: 'List Stytch data into an entity',
+  },
+  remove_user: {
+    desc: 'Remove Stytch data from an entity',
+  }
+
 
 }
 
-export default docs
+const sections = {}
+
+export default {
+  messages,
+  sections
+}
 
 if ('undefined' !== typeof (module)) {
-  module.exports = docs
+  module.exports = {
+    messages,
+    sections
+  }
 }
